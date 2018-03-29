81515a55ffd2ffb44a74891f5d8aa3be5d0bf29b
Songzhe Zhu <songzhezhu@email.arizona.edu>
2018-03-29 11:45:20 -0700

    data structure

--- phase4/phase4.c
+++ phase4/phase4.c
@@ -9,6 +9,29 @@ int  semRunning;
 
 int  ClockDriver(char *);
 int  DiskDriver(char *);
+void isKernelMode(char *);
+void setUserMode();
+void initProc(int);
+void initDiskQueue(diskQueue*);
+void addDiskQ(diskQueue*, procPtr);
+procPtr peekDiskQ(diskQueue*);
+procPtr removeDiskQ(diskQueue*);
+
+/* Globals */
+procStruct ProcTable[MAXPROC];
+
+int diskZapped; // indicates if the disk drivers are 'zapped' or not
+diskQueue diskQs[USLOSS_DISK_UNITS]; // queues for disk drivers
+int diskPids[USLOSS_DISK_UNITS]; // pids of the disk drivers
+
+// mailboxes for terminal device
+int charRecvMbox[USLOSS_TERM_UNITS]; // receive char
+int charSendMbox[USLOSS_TERM_UNITS]; // send char
+int lineReadMbox[USLOSS_TERM_UNITS]; // read line
+int lineWriteMbox[USLOSS_TERM_UNITS]; // write line
+int pidMbox[USLOSS_TERM_UNITS]; // pid to block
+int termInt[USLOSS_TERM_UNITS]; // interupt for term (control writing)
+int termProcTable[USLOSS_TERM_UNITS][3]; // keep track of term procs
 
 void
 start3(void)
@@ -22,6 +45,29 @@ start3(void)
     /*
      * Check kernel mode here.
      */
+    isKernelMode("start3");
+
+    // initialize proc table
+    for (i = 0; i < MAXPROC; i++) {
+        initProc(i);
+    }
+    
+    // initialize systemCallVec
+    systemCallVec[SYS_SLEEP] = sleep;
+    systemCallVec[SYS_DISKREAD] = diskRead;
+    systemCallVec[SYS_DISKWRITE] = diskWrite;
+    systemCallVec[SYS_DISKSIZE] = diskSize;
+    systemCallVec[SYS_TERMREAD] = termRead;
+    systemCallVec[SYS_TERMWRITE] = termWrite;
+
+    // mboxes for terminal
+    for (i = 0; i < USLOSS_TERM_UNITS; i++) {
+        charRecvMbox[i] = MboxCreate(1, MAXLINE);
+        charSendMbox[i] = MboxCreate(1, MAXLINE);
+        lineReadMbox[i] = MboxCreate(10, MAXLINE);
+        lineWriteMbox[i] = MboxCreate(10, MAXLINE); 
+        pidMbox[i] = MboxCreate(1, sizeof(int));
+    }
 
     /*
      * Create clock device driver 
@@ -105,3 +151,170 @@ DiskDriver(char *arg)
 {
     return 0;
 }
+
+void isKernelMode(char *name)
+{
+    if( (USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0 ) {
+        USLOSS_Console("%s: called while in user mode, by process %d. Halting...\n", 
+             name, getpid());
+        USLOSS_Halt(1); 
+    }
+} 
+
+void setUserMode()
+{
+    USLOSS_PsrSet( USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE );
+}
+
+/* initializes proc struct */
+void initProc(int pid) {
+    requireKernelMode("initProc()"); 
+
+    int i = pid % MAXPROC;
+
+    ProcTable[i].pid = pid; 
+    ProcTable[i].mboxID = MboxCreate(0, 0);
+    ProcTable[i].blockSem = semcreateReal(0);
+    ProcTable[i].wakeTime = -1;
+    ProcTable[i].diskTrack = -1;
+    ProcTable[i].nextDiskPtr = NULL;
+    ProcTable[i].prevDiskPtr = NULL;
+}
+
+/* ------------------------------------------------------------------------
+  Functions for the Queue
+   ----------------------------------------------------------------------- */
+
+/* Initialize the given Queue */
+void initDiskQueue(diskQueue* q) {
+    q->head = NULL;
+    q->tail = NULL;
+    q->curr = NULL;
+    q->size = 0;
+}
+
+/* Adds the proc pointer to the disk queue in sorted order */
+void addDiskQ(diskQueue* q, procPtr p) {
+    if (debug4)
+        USLOSS_Console("addDiskQ: adding pid %d, track %d to queue\n", p->pid, p->diskTrack);
+
+    // first add
+    if (q->head == NULL) { 
+        q->head = q->tail = p;
+        q->head->nextDiskPtr = q->tail->nextDiskPtr = NULL;
+        q->head->prevDiskPtr = q->tail->prevDiskPtr = NULL;
+    }
+    else {
+        // find the right location to add
+        procPtr prev = q->tail;
+        procPtr next = q->head;
+        while (next != NULL && next->diskTrack <= p->diskTrack) {
+            prev = next;
+            next = next->nextDiskPtr;
+            if (next == q->head)
+                break;
+        }
+        if (debug4)
+            USLOSS_Console("addDiskQ: found place, prev = %d\n", prev->diskTrack);
+        prev->nextDiskPtr = p;
+        p->prevDiskPtr = prev;
+        if (next == NULL)
+            next = q->head;
+        p->nextDiskPtr = next;
+        next->prevDiskPtr = p;
+        if (p->diskTrack < q->head->diskTrack)
+            q->head = p; // update head
+        if (p->diskTrack >= q->tail->diskTrack)
+            q->tail = p; // update tail
+    }
+    q->size++;
+    if (debug4)
+        USLOSS_Console("addDiskQ: add complete, size = %d\n", q->size);
+} 
+
+/* Returns the next proc on the disk queue */
+procPtr peekDiskQ(diskQueue* q) {
+    if (q->curr == NULL) {
+        q->curr = q->head;
+    }
+    return q->curr;
+}
+
+/* Returns and removes the next proc on the disk queue */
+procPtr removeDiskQ(diskQueue* q) {
+    if (q->size == 0)
+        return NULL;
+
+    if (q->curr == NULL) {
+        q->curr = q->head;
+    }
+
+    if (debug4)
+        USLOSS_Console("removeDiskQ: called, size = %d, curr pid = %d, curr track = %d\n", q->size, q->curr->pid, q->curr->diskTrack);
+
+    procPtr temp = q->curr;
+
+    if (q->size == 1) { // remove only node
+        q->head = q->tail = q->curr = NULL;
+    }
+
+    else if (q->curr == q->head) { // remove head
+        q->head = q->head->nextDiskPtr;
+        q->head->prevDiskPtr = q->tail;
+        q->tail->nextDiskPtr = q->head;
+        q->curr = q->head;
+    }
+
+    else if (q->curr == q->tail) { // remove tail
+        q->tail = q->tail->prevDiskPtr;
+        q->tail->nextDiskPtr = q->head;
+        q->head->prevDiskPtr = q->tail;
+        q->curr = q->head;
+    }
+
+    else { // remove other
+        q->curr->prevDiskPtr->nextDiskPtr = q->curr->nextDiskPtr;
+        q->curr->nextDiskPtr->prevDiskPtr = q->curr->prevDiskPtr;
+        q->curr = q->curr->nextDiskPtr;
+    }
+
+    q->size--;
+
+    if (debug4)
+        USLOSS_Console("removeDiskQ: done, size = %d, curr pid = %d, curr track = %d\n", q->size, temp->pid, temp->diskTrack);
+
+    return temp;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
